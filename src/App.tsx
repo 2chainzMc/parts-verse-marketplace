4e4b264ed0e6687d3087eb3517017a5721fd988d
2chainzMc <thulaning62@gmail.com>
2025-08-08 00:33:27 +0200

    more Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "@/components/Layout";
 import Index from "./pages/Index";
 import BuyPartsPage from "./pages/BuyPartsPage";
@@ -24,7 +23,7 @@ const App = () => (
       <UserProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Index />} />
@@ -38,7 +37,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </UserProvider>
     </TooltipProvider>
   </QueryClientProvider>
